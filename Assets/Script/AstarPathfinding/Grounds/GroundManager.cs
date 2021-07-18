25b5ea6c68291dc2f16f7f071d6bde64da20e124
TuHLT <renzokukens@yahoo.com>
2021-07-18 13:20:45 +0700

    - update

--- Assets/Script/AstarPathfinding/Grounds/GroundManager.cs
+++ Assets/Script/AstarPathfinding/Grounds/GroundManager.cs
@@ -36,6 +36,7 @@ namespace AStartPathfinding.Grounds
                 for (int j = 0; j < Height; j++)
                 {
                     var ground = Instantiate(m_groundCell, new Vector3(i, 0, j), m_groundCell.transform.rotation, transform);
+                    ground.gameObject.name = count.ToString();
                     ground.GetComponent<Renderer>().material = (i + j) % 2 == 0 ? m_matBlue : m_matYellow;
                     ground.GetComponent<Ground>().SetNumber(count);
                     count++;
